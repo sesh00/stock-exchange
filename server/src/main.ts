4087c5c15b7a99b24b4246cbc1e7071536ed2d78
sinne0r <ernestrsage@gmail.com>
2023-11-08 23:41:59 +0300

    1st stable version

--- server/src/main.ts
+++ server/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const httpServer = app.getHttpServer();
   const io = new Server(httpServer, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: ["http://localhost:3000", "http://localhost:5173"],
       methods: ["GET", "POST"]
     }
   });
